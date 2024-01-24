b5af80e47001c982d95a86d39da589319bde6389
Austin <austinjaguilar@gmail.com>
2024-01-24 13:30:12 -0800

    completed java mooc ii

--- part14-Part14_03.SavingsCalculator/src/main/java/application/SavingsCalculatorApplication.java
+++ part14-Part14_03.SavingsCalculator/src/main/java/application/SavingsCalculatorApplication.java
@@ -28,68 +28,75 @@ public class SavingsCalculatorApplication extends Application{
     
     @Override
     public void start(Stage window){
-        BorderPane layout = new BorderPane();
-        
-        VBox topHeading = new VBox();
-        topHeading.setPadding(new Insets(25));
+        BorderPane layout = new BorderPane();        
                 
-        BorderPane monthlySavings = new BorderPane(); 
+        //monthly sum slider
         Slider monthlySlider = new Slider(25, 250 ,4);
         monthlySlider.setShowTickMarks(true);
         monthlySlider.setShowTickLabels(true); 
-        Label sliderValue = new Label(String.valueOf(monthlySlider.getValue()));
+        monthlySlider.setSnapToTicks(true);
         
-        BorderPane interestRate = new BorderPane();
+        //interest slider
         Slider interestSlider = new Slider(0, 10, 0);
         interestSlider.setShowTickMarks(false);
         interestSlider.setShowTickLabels(true);
+        interestSlider.setSnapToTicks(true);
+        
+        //slider labels
+        Label sliderValue = new Label(String.valueOf(monthlySlider.getValue()));
         Label interestValue = new Label(String.valueOf(interestSlider.getValue()));
-          
+        
+        //setting borderPane object for monthly sum slider
+        BorderPane monthlySavings = new BorderPane();
         monthlySavings.setLeft(new Label("Monthly savings"));
         monthlySavings.setCenter(monthlySlider);
         monthlySavings.setRight(sliderValue);
+        
+        //setting borderPane object for interest slider
+        BorderPane interestRate = new BorderPane();
         interestRate.setLeft(new Label("Yearly interest rate"));
         interestRate.setCenter(interestSlider);
         interestRate.setRight(interestValue);
         
-        
+        //creating VBox object and adding the borderPane objects (interestRate and monthlySavings) to it
+        VBox topHeading = new VBox();
+        topHeading.setPadding(new Insets(25));
         topHeading.getChildren().addAll(monthlySavings, interestRate);       
-        
-        
+             
+        //setting numberAxis objects for LineChart
         NumberAxis xAxis = new NumberAxis(0, 30, 1);
         NumberAxis yAxis = new NumberAxis();
 
-        
+        //setting up LineChart and two XYCharts.Series objects that will display the savings and interest on the LineChart.
         LineChart<Number, Number> lineChart = new LineChart<>(xAxis, yAxis);
+        lineChart.setAnimated(false);
+        lineChart.setLegendVisible(false);
         XYChart.Series savingsChart = new XYChart.Series();
         XYChart.Series interestChart = new XYChart.Series();
 
-        
-        
+        //setting up title of LineChart and adding the XYCharts to the lineChart
         lineChart.setTitle("Savings Counter");
-        
         lineChart.getData().addAll(savingsChart, interestChart);
-
         
-        DecimalFormat df = new DecimalFormat("#.00");       
-
+        
+        //Adding a event listener to the monthly slider where it will update savingsChart according to the new value
         monthlySlider.valueProperty().addListener((observable, oldValue, newValue) -> {         
-            sliderValue.setText(String.format("%.2f", newValue.doubleValue()));            
-            
+            sliderValue.setText(String.format("%.2f", newValue.doubleValue()));                        
             updateChart(monthlySlider.getValue(), interestSlider.getValue(), savingsChart, interestChart);            
            
         });
         
+        //Adding a event listener to the interest slider where it will update savingsChart according to the new value
         interestSlider.valueProperty().addListener((observable, oldValue, newValue) -> {            
             interestValue.setText(String.format("%.2f", newValue.doubleValue()));           
-            
             updateChart(monthlySlider.getValue(),interestSlider.getValue(), savingsChart, interestChart);            
            
         });
         
+        //adding this so the graph will display the values on start of the program
         updateChart(monthlySlider.getValue(),interestSlider.getValue(), savingsChart, interestChart);            
 
-              
+        
         layout.setCenter(lineChart);
         layout.setTop(topHeading);
         
@@ -99,9 +106,12 @@ public class SavingsCalculatorApplication extends Application{
     }
     
     public void updateChart(double savings, double interest, XYChart.Series savingsSeries, XYChart.Series withInterestSeries){
+        
+        //clears the chart 
         savingsSeries.getData().clear();
         withInterestSeries.getData().clear();
         
+        //updates it with new values
         savingsSeries.getData().add(new XYChart.Data(0, 0));
         withInterestSeries.getData().add(new XYChart.Data(0, 0));   
         
@@ -128,151 +138,83 @@ public class SavingsCalculatorApplication extends Application{
         BorderPane display = new BorderPane();
 
  
-
         Slider monthlySum = new Slider();
-
         monthlySum.setMin(25);
-
         monthlySum.setMax(250);
-
         monthlySum.setSnapToTicks(true);
-
         monthlySum.setBlockIncrement(500);
-
         monthlySum.setShowTickLabels(true);
-
         monthlySum.setShowTickMarks(true);
 
  
-
         Slider interestRate = new Slider();
-
         interestRate.setMin(0);
-
         interestRate.setMax(10);
-
         interestRate.setMinorTickCount(10);
-
         interestRate.setShowTickLabels(true);
-
         interestRate.setShowTickMarks(true);
 
- 
-
         Label savingsText = new Label("25");
-
         Label interestRateText = new Label("0");
 
- 
-
         BorderPane monthlySumDisplay = new BorderPane();
-
         monthlySumDisplay.setLeft(new Label("Monthly savings"));
-
         monthlySumDisplay.setCenter(monthlySum);
-
         monthlySumDisplay.setRight(savingsText);
-
         monthlySumDisplay.setPadding(new Insets(10));
 
- 
-
         BorderPane interestRateDisplay = new BorderPane();
-
         interestRateDisplay.setLeft(new Label("Yearly interest rate"));
-
         interestRateDisplay.setCenter(interestRate);
-
         interestRateDisplay.setRight(interestRateText);
-
         interestRateDisplay.setPadding(new Insets(10));
 
- 
 
         VBox sliderDisplay = new VBox();
-
         sliderDisplay.getChildren().add(monthlySumDisplay);
-
         sliderDisplay.getChildren().add(interestRateDisplay);
 
- 
-
         sliderDisplay.setPadding(new Insets(10));
-
         sliderDisplay.setSpacing(10);
-
- 
 
         display.setTop(sliderDisplay);
 
- 
 
         NumberAxis xAxis = new NumberAxis(0, 30, 1);
-
         NumberAxis yAxis = new NumberAxis();
 
- 
-
         LineChart<Number, Number> savingsLineChart = new LineChart<>(xAxis, yAxis);
-
         savingsLineChart.setAnimated(false);
-
         savingsLineChart.setLegendVisible(false);
-
         savingsLineChart.setTitle("Savings calculator");
-
- 
 
         display.setCenter(savingsLineChart);
 
- 
 
         XYChart.Series savings = new XYChart.Series();
-
         XYChart.Series savingsWithInterest = new XYChart.Series();
 
- 
-
         savingsLineChart.getData().add(savings);
-
         savingsLineChart.getData().add(savingsWithInterest);
-
  
-
         monthlySum.setOnMouseReleased(e -> {
-
             savingsText.setText("" + monthlySum.getValue());
-
             updateChart(monthlySum.getValue(), interestRate.getValue(), savings, savingsWithInterest);
 
         });
-
- 
 
         interestRate.setOnMouseReleased(e -> {
-
- 
-
             String interestString = "" + interestRate.getValue();
-
             if (interestString.length() > 4) {
-
                 interestString = interestString.substring(0, 4);
-
             }
-
             interestRateText.setText(interestString);
-
             updateChart(monthlySum.getValue(), interestRate.getValue(), savings, savingsWithInterest);
-
         });
 
- 
 
         Scene view = new Scene(display, 640, 480);
-
         ikkuna.setScene(view);
-
         ikkuna.show();
 
     }
@@ -282,32 +224,17 @@ public class SavingsCalculatorApplication extends Application{
     private void updateChart(double monthlySavings, double interestRate, XYChart.Series savings, XYChart.Series savingsWithInterest) {
 
         savings.getData().clear();
-
         savingsWithInterest.getData().clear();
 
- 
-
         savings.getData().add(new XYChart.Data(0, 0));
-
         savingsWithInterest.getData().add(new XYChart.Data(0, 0));
-
- 
 
         double previous = 0;
 
- 
-
         for (int i = 1; i <= 30; i++) {
-
             savings.getData().add(new XYChart.Data(i, i * monthlySavings * 12));
-
- 
-
             previous += monthlySavings * 12;
-
             previous *= (1 + interestRate / 100.0);
-
- 
 
             savingsWithInterest.getData().add(new XYChart.Data(i, previous));
 
--- part14-Part14_09.Asteroids/src/main/java/asteroids/AsteroidsApplication.java
+++ part14-Part14_09.Asteroids/src/main/java/asteroids/AsteroidsApplication.java
@@ -1,14 +1,168 @@
 package asteroids;
 
-public class AsteroidsApplication {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+import javafx.animation.AnimationTimer;
+import javafx.application.Application;
+import javafx.geometry.Point2D;
+import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Polygon;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+public class AsteroidsApplication extends Application{
+    
+    public static int WIDTH = 600;
+    public static int HEIGHT = 400;
+    
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Pane pane = new Pane();
+        pane.setPrefSize(WIDTH, HEIGHT);
+        
+        //adding points text
+        Text text = new Text(10, 20, "Points: 0");
+        pane.getChildren().add(text);
+        
+        AtomicInteger points = new AtomicInteger();
+        
+        //Creates the ship and adds to the middle of the window
+        Ship ship = new Ship(WIDTH / 2, HEIGHT / 2);
+        
+        //Creates asteroids and adds them to a list
+        List<Asteroid> asteroids = new ArrayList<>();        
+        for (int i = 0; i < 5; i++){
+            Random rnd = new Random();
+            Asteroid asteroid = new Asteroid(rnd.nextInt(WIDTH), rnd.nextInt(HEIGHT));
+            asteroids.add(asteroid);
+        }
+        
+        //create list for projectiles
+        List<Projectile> projectiles = new ArrayList<>();
+          
+        pane.getChildren().add(ship.getCharacter());           
+        asteroids.forEach(asteroid -> pane.getChildren().add(asteroid.getCharacter()));
+        
+        Scene scene = new Scene(pane);
+        
+        //Create a HashMap to store the pressed keys. Used so the program can handle the keypresses as multiple events
+        Map<KeyCode, Boolean> pressedKeys = new HashMap<>();
+        
+        //Key press events
+        scene.setOnKeyPressed(event -> {
+            pressedKeys.put(event.getCode(), Boolean.TRUE);
+        });
+        
+        scene.setOnKeyReleased(event -> {
+            pressedKeys.put(event.getCode(), Boolean.FALSE);
+        });
+        
+        //abstract class is ran continously to process the key presses
+        new AnimationTimer() {
+            
+            @Override
+            public void handle(long now){            
+                //movement logic
+                if (pressedKeys.getOrDefault(KeyCode.LEFT, false)){
+                    ship.turnLeft();
+                }
+                
+                if (pressedKeys.getOrDefault(KeyCode.RIGHT, false)){
+                    ship.turnRight();
+                }
+                
+                if (pressedKeys.getOrDefault(KeyCode.UP, false)){
+                    ship.accelerate();
+                }
+                
+                //projectile logic for projectile speed and keypress and how many projectiles allowed on screen
+                if (pressedKeys.getOrDefault(KeyCode.SPACE, false) && projectiles.size() < 3) {
+                    // we shoot
+                    Projectile projectile = new Projectile((int) ship.getCharacter().getTranslateX(), (int) ship.getCharacter().getTranslateY());
+                    projectile.getCharacter().setRotate(ship.getCharacter().getRotate());
+                    projectiles.add(projectile);
+
+                    projectile.accelerate();
+                    projectile.setMovement(projectile.getMovement().normalize().multiply(3));
+
+                    pane.getChildren().add(projectile.getCharacter());
+                }
+                
+                ship.move();
+                asteroids.forEach(asteroid -> asteroid.move());
+                projectiles.forEach(projectile -> projectile.move());
+                
+                //if ship hits asteroid. stop program.
+                asteroids.forEach(asteroid -> {
+                    if (ship.collide(asteroid)) {
+                        stop();
+                    }
+                });
+                
+                //remove projectile and asteroid if they collide               
+                projectiles.forEach(projectile -> {
+                    asteroids.forEach(asteroid -> {
+                        if(projectile.collide(asteroid)) {
+                            projectile.setAlive(false);
+                            asteroid.setAlive(false);
+                        }
+                    });
+                    //set points for when player destroys asteroid
+                    if (!projectile.isAlive()){
+                        text.setText("Points: " + points.addAndGet(10));
+                    }
+                });
+                
+                projectiles.stream()
+                        .filter(projectile -> !projectile.isAlive())
+                        .forEach(projectile -> pane.getChildren().remove(projectile.getCharacter()));
+                projectiles.removeAll(projectiles.stream()
+                                            .filter(projectile -> !projectile.isAlive())
+                                            .collect(Collectors.toList()));
+
+                asteroids.stream()
+                        .filter(asteroid -> !asteroid.isAlive())
+                        .forEach(asteroid -> pane.getChildren().remove(asteroid.getCharacter()));
+                asteroids.removeAll(asteroids.stream()
+                                            .filter(asteroid -> !asteroid.isAlive())
+                                            .collect(Collectors.toList()));
+                
+                if (Math.random()< 0.005) {
+                    Asteroid asteroid = new Asteroid(WIDTH, HEIGHT);
+                    if (!asteroid.collide(ship)){
+                        asteroids.add(asteroid);
+                        pane.getChildren().add(asteroid.getCharacter());
+                    }
+                }
+                
+
+            }
+            
+        }.start();
+        
+        
+        stage.setTitle("Asteroids!");
+        stage.setScene(scene);
+        stage.show();
+    }
 
     public static void main(String[] args) {
         System.out.println("Hello, world!");
+        launch(args);
     }
 
     public static int partsCompleted() {
         // State how many parts you have completed using the return value of this method
-        return 0;
+        return 4;
     }
 
 }
